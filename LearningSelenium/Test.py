6f91efe5d77fb56fbdc18c46f39099a9b354af35
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-16 18:57:29 +0600

    git commit

---
+++ LearningSelenium/Test.py
@@ -0,0 +1,16 @@
+#Using Chrome
+# Opening Browser with an URL and fetching the Title of the webpage
+# fixed DeprecationWarning: executable_path has been deprecated selenium
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+driver_service = Service(executable_path="C:\SeleniumWebDrivers\Google Chrome(32bit)\chromedriver.exe") #fixing DeprecationWarning
+#driver_service = Service(executable_path="ChromeDriverManager().install())
+driver=webdriver.Chrome(service=driver_service) #fixing DeprecationWarning
+#driver = webdriver.Chrome(executable_path="C:\SeleniumWebDrivers\Google Chrome(32bit)\chromedriver.exe")
+
+driver.get("https://www.rcvacademy.com")
+driver.maximize_window()
+print(driver.title)
+driver.close()
--- LearningSelenium/WebdriverManager(4).py
+++ LearningSelenium/WebdriverManager(4).py
@@ -1,8 +1,24 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#Mozilla Working
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+#google chrome not working
+#from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service as ChromeService
+#from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.firefox import GeckoDriverManager
+#browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("https://www.rcvacademy.com")
 driver.maximize_window()
 print(driver.title)
+driver.close()
+
+
+

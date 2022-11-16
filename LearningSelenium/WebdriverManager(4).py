d6fbbf4d00991901032961a90533a0e2f0480493
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-16 19:09:21 +0600

    git commit

--- LearningSelenium/WebdriverManager(4).py
+++ LearningSelenium/WebdriverManager(4).py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("https://www.rcvacademy.com")
